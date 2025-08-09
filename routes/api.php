82321cacfd61a180a67544060e54b28f72637c03
tarrek osama <tarrekosama@gmail.com>
2025-08-09 16:26:07 +0300

    add user controller and 4 API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -12,4 +13,10 @@ Route::post('/resend-verification', [AuthController::class, 'resendVerification'
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // User Profile Management APIs
+    Route::patch('/user/update', [UserController::class, 'update']);
+    Route::patch('/user/change-password', [UserController::class, 'changePassword']);
+    Route::post('/user/reset-data', [UserController::class, 'resetData']);
+    Route::patch('/settings/language', [UserController::class, 'updateLanguage']);
 });
